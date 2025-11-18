3622e7ebf4ddd106c68ba88b2a61163305000d67
PavanKumarBatta <214g1a3371@srit.ac.in>
2025-11-18 14:36:44 +0530

    Initial commit

---
+++ MessagingProject/src/main/java/com/cts/MessagingProjectApplication.java
@@ -0,0 +1,12 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingProjectApplication.class, args);
+	}
+}
